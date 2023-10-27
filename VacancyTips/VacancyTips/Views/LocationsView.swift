c8836b78493754b624268289d4865eebd3c61dc0
Fabricio Bahiense <fabriciobs2000@gmail.com>
2023-10-27 12:24:00 -0400

    map inserted

--- VacancyTips/VacancyTips/Views/LocationsView.swift
+++ VacancyTips/VacancyTips/Views/LocationsView.swift
@@ -6,16 +6,16 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationsView: View {
     
     @EnvironmentObject private var vm: LocationsViewModel
     
     var body: some View {
-        List {
-            ForEach(vm.locations) {
-                Text($0.name)
-            }
+        ZStack {
+            Map(coordinateRegion: $vm.mapRegion)
+                .ignoresSafeArea()
         }
     }
 }
